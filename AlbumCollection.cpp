f2ab002494e3b62a1980fc02bec76283cd34457f
Dylan Neve <dylanneve1@gmail.com>
2023-11-15 13:07:34 +0000

    Prepare for test case two

--- AlbumCollection.cpp
+++ AlbumCollection.cpp
@@ -1,3 +1,43 @@
 //
 // Created by neved on 15/11/2023.
 //
+
+#include <iostream>
+
+#include "AlbumCollection.h"
+
+void AlbumCollection::InsertAlbum(string artist, string title, int year) {
+    Album album(artist, title, year);
+}
+
+void AlbumCollection::Print() {
+}
+
+void AlbumCollection::PrintByTitleSubstring(string titleSubstring) {
+}
+
+void AlbumCollection::PrintByArtist(string artist) {
+}
+
+void AlbumCollection::PrintByYear(int year) {
+}
+
+bool AlbumCollection::ContainsTitle(string title) {
+}
+
+bool AlbumCollection::ContainsArtist(string artist) {
+}
+
+bool AlbumCollection::ContainsYear(int year) {
+}
+
+void AlbumCollection::DeleteAlbumByTitle(string titleToSearch) {
+}
+
+void AlbumCollection::DeleteAlbumsByArtist(string artistToSearch) {
+}
+
+int AlbumCollection::GetNumberAlbums() {
+    int ret;
+    return ret;
+}
--- main.cpp
+++ main.cpp
@@ -3,11 +3,20 @@
 #include "AlbumCollection.h"
 #include "Album.h"
 
+using namespace std;
+
+AlbumCollection db;
+
+void InsertAlbums(){
+    db.InsertAlbum("The Beatles", "Sgt Pepper's Lonely Hearts Club Band", 1967);
+    db.InsertAlbum("Jay-Z", "The Blueprint", 2001);
+    db.InsertAlbum("The White Stripes", "Elephant", 2003);
+    db.InsertAlbum("Dua Lipa", "Future Nostalgia", 2020);
+    db.InsertAlbum("Taylor Swift", "Red (Taylor's Version)", 2021);
+    db.InsertAlbum("The Beatles", "Yellow Submarine", 1969);
+    db.InsertAlbum("The Plagiarist", "Future Nostalgia", 2021);
+}
 int main() {
-    Album album1 ("The Beatles", "Sgt Pepper's Lonely Hearts Club Band", 1967);
-    Album album2 ("Jay-Z", "The Blueprint", 2001);
-    cout << "album1 == album2 ? " << (album1 == album2) << endl;
-    cout << "album1 < album2 ? " << (album1 < album2) << endl;
-    cout << "album1 > album2 ? " << (album2 < album1) << endl;
-    return 0;
+    InsertAlbums();
+    db.PrintByYear(2021);
 }
