78b1421ee2203d06f11d6167b364032f4387506d
Dylan Neve <dylanneve1@gmail.com>
2023-11-15 12:49:52 +0000

    Finish get artist, title and year and start operator funcs

--- Album.cpp
+++ Album.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <string>
 
 #include "Album.h"
 
@@ -10,4 +11,56 @@ Album::Album(std::string newArtist, std::string newTitle, int newYear) {
     this->artist = newArtist;
     this->title = newTitle;
     this->year = newYear;
+}
+
+std::string Album::GetArtist() const {
+    return this->artist;
+}
+
+std::string Album::GetTitle() const {
+    return this->title;
+}
+
+int Album::GetYear() const {
+    return this->year;
+}
+
+bool Album::operator==(const Album &itemToCompare) const {
+    bool ret;
+    if (this->artist == itemToCompare.artist) {
+        if (this->title == itemToCompare.title) {
+            if (this->year == itemToCompare.year) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+        } else {
+            ret = false;
+        }
+    } else {
+        ret = false;
+    }
+    return ret;
+}
+
+bool Album::operator<(const Album &itemToCompare) const {
+    bool ret;
+    if (this->artist.length() > itemToCompare.artist.length()) {
+        ret = true;
+    } else {
+        if (this->title.length() > itemToCompare.title.length()) {
+            ret = true;
+        } else {
+            if (this->year > itemToCompare.year) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+        }
+    }
+    return ret;
+}
+
+Album::operator const char *() const {
+    return nullptr;
 }
